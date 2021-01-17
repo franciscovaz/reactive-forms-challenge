27c43ab7c5b3d8fefa8dfb8b0b0173dfbc748da4
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-17 11:22:04 +0000

    created form with values validate username and email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatus: string[] = ['Stable', 'Critical', 'Finished'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('Stable')
+    })
+  }
+
+  onSubit() {
+    console.log(this.signupForm);
+  }
 }
